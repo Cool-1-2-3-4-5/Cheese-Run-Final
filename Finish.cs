01928833b72ce21cffbcd6a5085d4609568a8026
Cool-1-2-3-4-5 <126423230+Cool-1-2-3-4-5@users.noreply.github.com>
2024-01-08 21:09:29 -0500

    Add files via upload

---
+++ Finish.cs
@@ -0,0 +1,16 @@
+/*using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    public string gameScreen;
+    public string final;
+    void OnCollisionEnter(Collision player)
+    {
+        if(player.collider.name == final)
+        {
+            SceneManager.LoadScene(gameScreen);
+        }
+    }
+}
+*/
